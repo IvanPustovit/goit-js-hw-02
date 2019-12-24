a8ee01313961db301cd1d937b117a52d3e3390c4
IvanPustovit <pustovit.for@gmail.com>
2019-12-25 00:14:04 +0200

    Homework2_4

--- task-06.js
+++ task-06.js
@@ -4,18 +4,19 @@ let input;
 const numbers = [];
 let total = 0;
 
+
+
 do {
   input = prompt("Введите число");
-  if (input) {
+  if (input > 0 || input < 0) {
     numbers.push(Number(input));
   }
 } while (input !== null);
 
-for (const number of numbers) {
-  !!numbers.length;
-  total += number;
-}
-
-if (total > 0) {
+if (numbers.length > 0) {
+  for (const number of numbers) {
+    total += number
+  }
   console.log(total);
 }
+console.log(numbers);
